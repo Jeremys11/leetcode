9ac11ce52168ad5cd215fdad88b43daa799f609a
Jeremys11 <jeremysingh116@gmail.com>
2023-06-29 17:30:40 -0400

    remove duplicate 2

--- RemoveDuplicates2/removeDuplicates2.py
+++ RemoveDuplicates2/removeDuplicates2.py
@@ -1,4 +1,25 @@
 class Solution:
     def removeDuplicates(self, nums) -> int:
-        temp = nums
-        return 0
+
+        counts = {}
+        for i in range(len(nums)):
+            if nums[i] in counts:
+                counts[nums[i]] += 1
+            else:
+                counts[nums[i]] = 1
+
+        for key in counts:
+            print(counts[key])
+            while counts[key] > 2:
+                nums.remove(key)
+                counts[key] -= 1
+
+        return len(nums)
+    
+
+nums = [1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6]
+
+solution = Solution()
+solution.removeDuplicates(nums=nums)
+
+print(nums)

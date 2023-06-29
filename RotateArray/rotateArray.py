f64266e2d5de56334abe552d2ca90c4cf310dc9d
Jeremys11 <jeremysingh116@gmail.com>
2023-06-29 19:10:27 -0400

    rotateArray + majorityElement

---
+++ RotateArray/rotateArray.py
@@ -0,0 +1,23 @@
+class Solution:
+
+    def rotate(self, nums, k: int) -> None:
+
+        holding = {}
+        for i in range(len(nums)):
+            if i in holding:
+                newPosition = (i+k) % len(nums)
+                holding[newPosition] = nums[newPosition]
+                nums[newPosition] = holding[i]
+                holding.pop(i)
+            else:
+                newPosition = (i+k) % len(nums)
+                holding[newPosition] = nums[newPosition]
+                nums[newPosition] = nums[i]
+
+nums = [1,2,3,4,5]
+k = 6
+
+solution = Solution()
+solution.rotate(nums=nums,k=k)
+
+print(nums)
